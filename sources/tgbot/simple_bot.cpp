4046f4a2ae350eac068bdf1f72bf4f7b8d57183e
I.Kostiuk <gefestroyale@gmail.com>
2023-11-24 11:18:56 +0200

    SimpleBot implemented

---
+++ sources/tgbot/simple_bot.cpp
@@ -0,0 +1,162 @@
+#include "simple_bot.hpp"
+#include <core/move.hpp>
+#include <tgbot/net/TgLongPoll.h>
+
+KeyboardLayout Keyboard::ToKeyboard(ConstSpan<String> texts) {
+    KeyboardLayout layout;
+    List<KeyboardButton> row;
+
+    for (const auto &text : texts) {
+        row.Emplace(KeyboardButton(text));
+    }
+
+    layout.Emplace(std::move(row));
+
+    return layout;
+}
+
+TgBot::InlineKeyboardMarkup::Ptr ToInlineKeyboardMarkup(const KeyboardLayout &keyboard) {
+
+    TgBot::InlineKeyboardMarkup::Ptr keyboard_markup(new TgBot::InlineKeyboardMarkup);
+
+    for (const auto& row : keyboard) {
+        std::vector<TgBot::InlineKeyboardButton::Ptr> row_markup;
+
+        for (const auto& button : row) {
+            TgBot::InlineKeyboardButton::Ptr button_markup(new TgBot::InlineKeyboardButton);
+            button_markup->text = button.Text;
+            button_markup->callbackData = button.CallbackData;
+
+            row_markup.push_back(button_markup);
+        }
+
+        keyboard_markup->inlineKeyboard.push_back(row_markup);
+    }
+    
+    return keyboard_markup;
+}
+
+SimpleBot::SimpleBot(const String& token):
+    TgBot::Bot(token)
+{}
+
+int SimpleBot::Run() {
+    Log("Started");
+
+    try {
+        TgBot::TgLongPoll long_poll(*this);
+
+        while (true) {
+            long_poll.start();
+        }
+
+    }
+    catch (const std::exception& exception) {
+        Log("Crash: %", exception.what());
+    }
+
+    return EXIT_SUCCESS;
+}
+
+TgBot::Message::Ptr SimpleBot::SendMessage(const TgBot::Message::Ptr& source, const String& message) {
+    return SendMessage(source, message, nullptr);
+}
+
+TgBot::Message::Ptr SimpleBot::SendMessage(const TgBot::Message::Ptr& source, const String& message, TgBot::GenericReply::Ptr reply) {
+    if (!message.Size()) {
+        Log("Can't send empty messages");
+        return nullptr;
+    }
+
+    TgBot::Message::Ptr result = nullptr;
+
+    try {
+        result = getApi().sendMessage(source->chat->id, message, false, 0, reply, "", false, {}, false, false, source->isTopicMessage ? source->messageThreadId : 0);
+    }
+    catch (const std::exception& exception) {
+        auto chat = source->chat;
+
+        auto chat_name = chat->username.size() ? chat->username : chat->title;
+
+        Log("Failed to send message to chat '%' id % reason %", chat_name, chat->id, exception.what());
+    }
+
+    return result;
+}
+
+void SimpleBot::EditMessage(TgBot::Message::Ptr message, const String& text, const KeyboardLayout& keyboard) {
+    EditMessage(message, text, ToInlineKeyboardMarkup(keyboard));
+}
+
+TgBot::Message::Ptr SimpleBot::SendKeyboard(const TgBot::Message::Ptr& source, const String& message, const KeyboardLayout& keyboard) {
+    return SendMessage(source, message, ToInlineKeyboardMarkup(keyboard));
+}
+
+void SimpleBot::EditMessage(TgBot::Message::Ptr message, const String& text, TgBot::GenericReply::Ptr reply) {
+    try{
+        if (text.size()) {
+            getApi().editMessageText(text, message->chat->id, message->messageId, "", "", false, reply);
+        } else {
+            getApi().editMessageReplyMarkup(message->chat->id, message->messageId, "", reply);
+        }
+    }
+    catch (const std::exception& exception) {
+        Log("Failed to edit message: %", exception.what());
+    }
+}
+
+void SimpleBot::EditMessage(TgBot::Message::Ptr message, const KeyboardLayout& keyboard) {
+    EditMessage(message, String::Empty, ToInlineKeyboardMarkup(keyboard));
+}
+
+void SimpleBot::EditMessage(TgBot::Message::Ptr message, const String& text) {
+    EditMessage(message, text, nullptr);
+}
+
+bool SimpleBot::AnswerCallbackQuery(const std::string& callbackQueryId, const String& text) {
+    
+    try{
+        return getApi().answerCallbackQuery(callbackQueryId, text);
+    }
+    catch (const std::exception& exception) {
+        Log("Failed to answer callback query %", callbackQueryId);
+    }
+    return false;
+}
+
+void SimpleBot::DeleteMessage(TgBot::Message::Ptr message) {
+    try{
+        getApi().deleteMessage(message->chat->id, message->messageId);
+    }
+    catch (const std::exception& exception) {
+        auto chat = message->chat;
+
+        auto chat_name = chat->username.size() ? chat->username : chat->title;
+
+        Log("Failed to delete message % from chat %, id %, reason: %", message->messageId, chat_name, chat->id, exception.what());
+    }
+}
+
+void SimpleBot::BindOnMessage(Function<void(TgBot::Message::Ptr)> callback){
+    getEvents().onNonCommandMessage([&](auto message) { callback.TryCall(message); });
+}
+
+void SimpleBot::BindOnUnknownCommand(Function<void(TgBot::Message::Ptr)> callback) {
+    getEvents().onUnknownCommand([&](auto message) { callback.TryCall(message); });
+}
+
+void SimpleBot::BindOnCommand(const String &command, Function<void(TgBot::Message::Ptr)> callback) {
+    getEvents().onCommand(command, [&](auto message) { callback.TryCall(message); });
+}
+
+void SimpleBot::BindOnCommand(std::initializer_list<std::string> commands, Function<void(TgBot::Message::Ptr)> callback) {
+    getEvents().onCommand(commands, [&](auto message) { callback.TryCall(message); });
+}
+
+void SimpleBot::BindOnCallbackQuery(Function<void(TgBot::CallbackQuery::Ptr)> callback) {
+    getEvents().onCallbackQuery([&](auto query) { callback.TryCall(query); });
+}
+
+void SimpleBot::BindOnLog(Function<void(String)> callback) {
+    m_OnLog = callback;
+}
---
+++ sources/tgbot/simple_bot.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <core/string.hpp>
+#include <core/list.hpp>
+#include <core/function.hpp>
+#include <core/format.hpp>
+#include <core/span.hpp>
+#include <tgbot/Bot.h>
+
+struct KeyboardButton {
+    String Text;
+    String CallbackData;
+
+    KeyboardButton(KeyboardButton &&) = default;
+
+    KeyboardButton &operator=(KeyboardButton &&) = default;
+
+    KeyboardButton(String text, String callback_data) :
+        Text(Move(text)),
+        CallbackData(Move(callback_data))
+    {}
+
+    KeyboardButton(String text) :
+        Text(text),
+        CallbackData(text)
+    {}
+};
+
+using KeyboardLayout = List<List<KeyboardButton>>;
+
+namespace Keyboard {
+    KeyboardLayout ToKeyboard(ConstSpan<String> texts);
+}
+
+class SimpleBot: private TgBot::Bot {
+    Function<void(String)> m_OnLog;
+public:
+    SimpleBot(const String& token);
+
+    int Run();
+
+    TgBot::Message::Ptr SendMessage(const TgBot::Message::Ptr& source, const String& message);
+
+    TgBot::Message::Ptr SendMessage(const TgBot::Message::Ptr& source, const String& message, TgBot::GenericReply::Ptr reply);
+
+    TgBot::Message::Ptr SendKeyboard(const TgBot::Message::Ptr& source, const String& message, const KeyboardLayout& keyboard);
+
+    void EditMessage(TgBot::Message::Ptr message, const String &text, TgBot::GenericReply::Ptr reply);
+
+    void EditMessage(TgBot::Message::Ptr message, const String &text, const KeyboardLayout &keyboard);
+
+    void EditMessage(TgBot::Message::Ptr message, const KeyboardLayout &keyboard);
+
+    void EditMessage(TgBot::Message::Ptr message, const String &text);
+
+    void DeleteMessage(TgBot::Message::Ptr message);
+
+    bool AnswerCallbackQuery(const std::string& callbackQueryId, const String& text = String::Empty);
+    
+    template<typename...ArgsType>
+    void Log(const char *fmt, const ArgsType&...args) {
+        m_OnLog(Format(fmt, args...));
+    }
+
+    void BindOnMessage(Function<void(TgBot::Message::Ptr)> callback);
+
+    void BindOnUnknownCommand(Function<void(TgBot::Message::Ptr)> callback);
+
+    void BindOnCommand(const String &command, Function<void(TgBot::Message::Ptr)> callback);
+
+    void BindOnCommand(std::initializer_list<std::string> commands, Function<void(TgBot::Message::Ptr)> callback);
+
+    void BindOnCallbackQuery(Function<void(TgBot::CallbackQuery::Ptr)> callback);
+
+    void BindOnLog(Function<void(String)> callback);
+};
